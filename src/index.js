b3838de2034b4ffdbae544329070bccf83698e76
goder123 <reviol697@gmail.com>
2022-06-29 16:07:09 +0400

    feat:finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n * -1;
+  }
+    var m = n.toString().split("");
+    var v = m.reverse();
+    var p = v.join("");
+    return p;
+
 }
